17f9248c6551ef3e2d2730da0392f2b01e31a3f2
kayal432 <132423987+kayal432@users.noreply.github.com>
2023-05-04 17:39:19 +0530

    Update functions.py

--- functions.py
+++ functions.py
@@ -1,8 +1,11 @@
-from telegram import *
-from telegram.ext import *
-from telegram.ext import Updater
-import telegram
+# from telegram import *
+# from telegram.ext import *
+# from telegram.ext import Updater
+# import telegram
+# from telegram.ext import Updater, CommandHandler, MessageHandler
 from telegram.ext import Updater, CommandHandler, MessageHandler
+from telegram,bot import Bot
+
 
 bot_token='6061292744:AAEEurmseHxL4nzKnCr4nQzKHyH16kmq24Y'
 
