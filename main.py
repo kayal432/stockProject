ef61ba0d8064ebf054ac6ca531ab0bc88ac73de4
kayal432 <132423987+kayal432@users.noreply.github.com>
2023-05-04 19:48:24 +0530

    Update main.py

--- main.py
+++ main.py
@@ -11,18 +11,12 @@ def start(update,context):
     text=message.text
     bot.sendMessage(chat_id=chat_id,text='hoii')
 bot = Bot(token=bot_token)
+photo_url = 'https://images.app.goo.gl/Qw8S9X19KXJr37FT7'
 
-# The URL of the image you want to use as the bot's profile picture
-image_url = 'https://images.app.goo.gl/Qw8S9X19KXJr37FT7'
-
-# Download the image from the URL and save it locally
-image_data = requests.get(image_url).content
-with open('profile_photo.jpg', 'wb') as f:
-    f.write(image_data)
-
-# Set the profile picture using the saved image file
-with open('profile_photo.jpg', 'rb') as f:
-    bot.set_profile_photo(photo=f)
+response = requests.get(photo_url)
+if response.status_code == 200:
+    photo_file = BytesIO(response.content)
+    bot.set_profile_photo(photo=photo_file)
 
 def main():
     updater = Updater(token=bot_token, use_context=True)
