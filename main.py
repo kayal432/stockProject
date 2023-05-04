ebd1750a66a66e6ec6a32a4944e5ad8150675f52
kayal432 <132423987+kayal432@users.noreply.github.com>
2023-05-04 17:18:24 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,19 @@
-print("hii")
+
+
+from functions import *
+
+
+def start(update,context):
+    message=update.message
+    chat_id=message.chat_id
+    text=message.text
+    bot.sendMessage(chat_id=chat_id,text='hoii')
+
+def main():
+    updater = Updater(token=bot_token, use_context=True)
+
+    dp=updater.dispatcher
+    dp.add_handler(CommandHandler('start',start))
+    updater.start_polling()
+
+main()
