3957402937b15bbd40f82cad12114927d1167d56
kayal432 <132423987+kayal432@users.noreply.github.com>
2023-05-04 17:19:01 +0530

    Create functions.py

---
+++ functions.py
@@ -0,0 +1,9 @@
+from telegram import *
+from telegram.ext import *
+from telegram.ext import Updater
+import telegram
+from telegram.ext import Updater, CommandHandler, MessageHandler
+
+bot_token='6061292744:AAEEurmseHxL4nzKnCr4nQzKHyH16kmq24Y'
+
+bot=Bot(token=bot_token)
