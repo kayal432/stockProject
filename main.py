bbe9afd09e2ac97300dfee5c6eca9ec957dea0a6
kayal432 <132423987+kayal432@users.noreply.github.com>
2023-05-05 06:40:45 +0530

    Update main.py

--- main.py
+++ main.py
@@ -10,19 +10,20 @@ def start(update,context):
     chat_id=message.chat_id
     text=message.text
     bot.sendMessage(chat_id=chat_id,text='hoii')
-bot = Bot(token=bot_token)
-photo_url = 'https://images.app.goo.gl/Qw8S9X19KXJr37FT7'
-
-response = requests.get(photo_url)
-if response.status_code == 200:
-    photo_file = BytesIO(response.content)
-    bot.set_profile_photo(photo=photo_file)
+    
+def hii(update,context):
+    message=update.message
+    chat_id=message.chat_id
+    text=message.text
+    bot.sendMessage(chat_id=chat_id,text='bye')
+ 
 
 def main():
     updater = Updater(token=bot_token, use_context=True)
 
     dp=updater.dispatcher
     dp.add_handler(CommandHandler('start',start))
+    dp.add_handler(CommandHandler('hii',hii))
     updater.start_polling()
 
 main()
