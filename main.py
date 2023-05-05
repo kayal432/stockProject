044c5a3a2f4a9b40f88796d91221460e71ebb54a
kayal432 <132423987+kayal432@users.noreply.github.com>
2023-05-05 06:55:30 +0530

    Update main.py

--- main.py
+++ main.py
@@ -4,16 +4,23 @@ import requests
 
 from functions import *
 
-
+#start command
 def start(update,context):
     message=update.message
     chat_id=message.chat_id
     text=message.text
     bot.sendMessage(chat_id=chat_id,text='hoii')
-    
+#help command    
 def help(update,context):
    update.message.reply_text("hiii,how can i help you")
- 
+
+
+def fallback(update: Update, context):
+    # your fallback handler code here
+    message = update.message
+    text = message.text
+    chat_id = message.chat_id
+    bot.send_message(chat_id=chat_id, text="I'm sorry, I didn't understand that command.")
 
 def main():
     updater = Updater(token=bot_token, use_context=True)
@@ -21,6 +28,7 @@ def main():
     dp=updater.dispatcher
     dp.add_handler(CommandHandler('start',start))
     dp.add_handler(CommandHandler('help',help))
+    dp.add_handler(MessageHandler(Filters.command, fallback))
     updater.start_polling()
 
 main()
