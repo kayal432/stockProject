21eab5a85b04f977b74ce5d33f965cc37386e45e
kayal432 <132423987+kayal432@users.noreply.github.com>
2023-05-05 07:27:25 +0530

    Update main.py

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ def start(update,context):
     message=update.message
     chat_id=message.chat_id
     text=message.text
-    bot.sendMessage(chat_id=chat_id,text='hoii')
+    bot.send_message(chat_id=chat_id, text=f"Hi {username}")
 #help command    
 def help(update,context):
    update.message.reply_text("hiii,how can i help you")
@@ -20,11 +20,16 @@ def fallback(update: Updater, context):
     bot.send_message(chat_id=chat_id, text="I'm sorry, I didn't understand that command.")
     
    
-def message(update,context):
+def command(update,context):
     message = update.message
     chat_id = message.chat_id
     text = message.text
-    bot.send_message(chat_id=chat_id, text=text)
+    comm="""/hii
+        /help
+        """
+    
+    bot.send_message(chat_id=chat_id, text=comm)
+ 
         
 
 def main():
@@ -33,6 +38,7 @@ def main():
     dp=updater.dispatcher
     dp.add_handler(CommandHandler('start',start))
     dp.add_handler(CommandHandler('help',help))
+    dp.add_handler(CommandHandler('get_command',command))
     dp.add_handler(MessageHandler(Filters.command, fallback))
     dp.add_handler(MessageHandler(Filters.text & ~Filters.command, message))
     updater.start_polling()
