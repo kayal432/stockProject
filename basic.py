238766169e11d2503f79f2df25bbfcc346ca1c4f
kayal432 <132423987+kayal432@users.noreply.github.com>
2023-05-07 15:40:41 +0530

    Update basic.py

--- basic.py
+++ basic.py
@@ -2,19 +2,29 @@ from functions import *
 
 add_item,update_item,remove_item,show_item = range(4)
 
-def today_date(update,context):
-  now = datetime.now()
-  date_str = now.strftime("%Y-%m-%d") # format the date as YYYY-MM-DD
-  bot.send_message(chat_id=update.message.chat_id, text=f"The current date is {date_str}")
-  
-  
-def add_item():
-   bot.send_message(chat_id=update.message.chat_id, text='hii')
-   return ConversationHandler.END
+ADD_ITEM, REMOVE_ITEM, UPDATE_ITEM = range(3)
 
-def remove_item():
-   bot.send_message(chat_id=update.message.chat_id, text='hii')
-   return ConversationHandler.END
-def update_item():
-   bot.send_message(chat_id=update.message.chat_id, text='hii')
-   return ConversationHandler.END
+def basic(update, context):
+    user = update.message.from_user
+    update.message.reply_text(f"Hi {user.first_name}! What would you like to do today?", reply_markup=ReplyKeyboardMarkup([['Add item'], ['Remove item'], ['Update item']], one_time_keyboard=True))
+    return ADD_ITEM
+
+def add_item(update, context):
+    # Your code for add_item function goes here
+    update.message.reply_text("You have chosen to add an item!")
+    return ConversationHandler.END
+
+def remove_item(update, context):
+    # Your code for remove_item function goes here
+    update.message.reply_text("You have chosen to remove an item!")
+    return ConversationHandler.END
+
+def update_item(update, context):
+    # Your code for update_item function goes here
+    update.message.reply_text("You have chosen to update an item!")
+    return ConversationHandler.END
+
+def cancel(update, context):
+    user = update.message.from_user
+    update.message.reply_text(f"Bye {user.first_name}! Hope to see you again soon.", reply_markup=ReplyKeyboardRemove())
+    return ConversationHandler.END
