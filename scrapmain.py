473fdb31dcbea9414a8cab97827df4dd4f908253
kayal432 <132423987+kayal432@users.noreply.github.com>
2023-05-07 11:00:42 +0530

    Update scrapmain.py

--- scrapmain.py
+++ scrapmain.py
@@ -6,4 +6,6 @@ from bs4 import BeautifulSoup
 import requests
 
 page_main=requests.get('https://www.google.com/search?q=exide+stock+price&rlz=1C1YTUH_enIN990IN990&oq=exide+stock+&aqs=chrome.1.69i57j0i512l9.8008j1j7&sourceid=chrome&ie=UTF-8').text
-print(page_main)
+soup=BeautifulSoup(page_main,'lxml')
+j=soup.find('span',class_='IsqQVc NprOob wT3VGc').text
+print(j)
