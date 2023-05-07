ea3787a5db2724a9e1d486f58bc287da6023298c
kayal432 <132423987+kayal432@users.noreply.github.com>
2023-05-07 11:16:02 +0530

    Update scrapmain.py

--- scrapmain.py
+++ scrapmain.py
@@ -1,12 +1,24 @@
-from functions import *
-
-from bs4 import BeautifulSoup 
-
-
 import requests
+from bs4 import BeautifulSoup
 
-page_main=requests.get('https://groww.in/stocks/exide-industries-ltd')
-print(page_main)
-# soup=BeautifulSoup(page_main,'lxml')
-# j=soup.find('span',class_='lpu38Pri fs28 ')
-# print(j)
+def get_stock_price(company):
+    # Construct the Google search URL
+    url = f"https://www.google.com/search?q={company}+stock+price"
+
+    # Send a GET request to the URL
+    response = requests.get(url)
+
+    # Parse the HTML content of the response
+    soup = BeautifulSoup(response.content, "html.parser")
+
+    # Find the stock price in the parsed HTML content
+    try:
+        stock_price = soup.find("div", class_="BNeawe iBp4i AP7Wnd").get_text()
+    except:
+        stock_price = f"Unable to get stock price for {company}"
+
+    return stock_price
+
+# Example usage
+stock_price = get_stock_price("Tesla")
+print(stock_price)
