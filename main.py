90ee9d2d2ee18ce3f61fde38f35deaf0dc15220d
kayal432 <132423987+kayal432@users.noreply.github.com>
2023-05-07 11:36:13 +0530

    Update main.py

--- main.py
+++ main.py
@@ -66,9 +66,9 @@ def start(update,context):
     message=update.message
     chat_id=message.chat_id
     tesla=['tesla','exide']
+    text=message.text
     for i in tesla:
         username=get_stock_price(tesla)
-        text=message.text
         bot.send_message(chat_id=chat_id, text=f"{i} price is {username}")
     
     
