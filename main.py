4d488b52155cc32a0b1e017e0831ac201204cea7
kayal432 <132423987+kayal432@users.noreply.github.com>
2023-05-07 15:13:41 +0530

    Update main.py

--- main.py
+++ main.py
@@ -65,7 +65,7 @@ def main():
     create = ConversationHandler(
         entry_points=[CommandHandler('today_day',today_date)],
         states={
-            CHAT_ID: [MessageHandler(Filters.text, chat_id)],
+   
         }, fallbacks=[]
     )
    
