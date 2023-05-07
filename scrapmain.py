85ffa60deb667b499280b9e6838993dd8555d703
kayal432 <132423987+kayal432@users.noreply.github.com>
2023-05-07 10:42:36 +0530

    Update scrapmain.py

--- scrapmain.py
+++ scrapmain.py
@@ -1,8 +1,8 @@
 from functions import *
 
-from bs4 import beautifulSoup
+from BeatifulSoup4 import beautifulSoup
 
 import requests
-def scrap():
-  page_main=requests.get('https://www.google.com/search?q=exide+stock+price&rlz=1C1YTUH_enIN990IN990&oq=exide+stock+&aqs=chrome.1.69i57j0i512l9.8008j1j7&sourceid=chrome&ie=UTF-8').text
-  print(page_main)
+
+page_main=requests.get('https://www.google.com/search?q=exide+stock+price&rlz=1C1YTUH_enIN990IN990&oq=exide+stock+&aqs=chrome.1.69i57j0i512l9.8008j1j7&sourceid=chrome&ie=UTF-8').text
+print(page_main)
